764505da56513b1d57767ce27a0233089387ee73
Mustafa Pašić <62052767+mpasic2@users.noreply.github.com>
2021-03-17 19:03:25 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+
+import javafx.scene.control.Label;
+
+public class Controller {
+
+}
---
+++ src/sample/SampleController.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class SampleController {
+    public Label lblHW;
+
+    public void prikaziHelloWorld(ActionEvent actionEvent) {
+        lblHW.setText("Hello world");
+
+
+
+    }
+}
