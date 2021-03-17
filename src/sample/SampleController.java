ab78e2d1df04564b132b2627ce18ae5afc525227
Mustafa Pašić <62052767+mpasic2@users.noreply.github.com>
2021-03-17 19:12:45 +0100

    added red color

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -2,12 +2,16 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 public class SampleController {
     public Label lblHW;
+    public GridPane gridBackground;
 
     public void prikaziHelloWorld(ActionEvent actionEvent) {
         lblHW.setText("Hello world");
+        gridBackground.setStyle("-fx-background-color: red");
 
 
 
